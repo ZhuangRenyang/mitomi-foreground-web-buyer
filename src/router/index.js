c2f8158c3eda06f90e96249f351494ee9b383e91
ZhuangRenyang <996491835@qq.com>
2022-11-30 16:33:23 +0800

    0.0.2

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,63 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/mitomi',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    //重定向
+    redirect:'mitomi/techno',
+    children:[
+      {
+        path: 'techno',
+        component: () => import('../views/techno/TechnoView'),
+        meta: {
+          title: '米To米-手机'
+        }
+      },
+      {
+        path: 'clothing',
+        component:() => import('../views/clothing/ClothingView'),
+        meta: {
+          title: '米To米-服装'
+        }
+      },
+      {
+        path: 'food',
+        component:() => import('../views/food/FoodView'),
+        meta: {
+          title: '米To米-食品'
+        }
+      },
+      {
+        path: 'fresh',
+        component:() => import('../views/fresh/FreshView'),
+        meta: {
+          title: '米To米-生鲜'
+        }
+      },
+      {
+        path: 'appliance',
+        component:() => import('../views/appliance/ApplianceView'),
+        meta: {
+          title: '米To米-家电'
+        }
+      },
+      {
+        path: 'industrialProducts',
+        component:() => import('../views/industrialProducts/IndustrialProductsView'),
+        meta: {
+          title: '米To米-工业品'
+        }
+      },
+    ],
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
   }
 ]
 
@@ -27,3 +73,9 @@ const router = new VueRouter({
 })
 
 export default router
+
+router.beforeEach((to, from, next) => {
+
+  document.title = to.meta.title ? to.meta.title : "米To米商城";
+  next();
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,42 @@
 <!--    <img alt="Vue logo" src="../assets/logo.png">-->
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <!-- 蓝色导航条开始 -->
+        <div style="width: 100%;height: 60px;background-color: #82c8ec">
+          <el-menu style="width: 1200px;margin: 0 auto"
+                   default-active="1"
+                   class="el-menu-demo"
+                   mode="horizontal"
+                   @select="handleSelect"
+                   background-color="#82c8ec"
+                   text-color="#fff"
+                   active-text-color="#fff">
+            <el-menu-item v-for="c in categoryArr" :index="c.id">{{c.name}}</el-menu-item>
+            <!-- 搜索框开始 -->
+            <div style="float: right;margin-top: 15px">
+              <el-input size="mini" placeholder="请输入关键字搜索"></el-input>
+              <el-button style="position: absolute;background-color: rgba(0,0,0,0);border: 0;right: 0;top: 11px"
+                         icon="el-icon-search"></el-button>
+            </div>
+            <!-- 搜索框结束 -->
+          </el-menu>
+        </div>
+        <!-- 蓝色导航条结束 -->
+        <div style="width: 1200px;margin: 0 auto">
+          <img src="imgs/logo.png" style="width: 300px;vertical-align: middle">
+          <span>
+                    <a index='/mitomi/techno'>手机</a><el-divider direction="vertical"></el-divider>
+                    <a index=''>热点咨询</a><el-divider direction="vertical"></el-divider>
+                    <a index=''>商家入驻</a><el-divider direction="vertical"></el-divider>
+                    <a index=''>社会招聘</a><el-divider direction="vertical"></el-divider>
+                    <a index=''>校园招聘</a><el-divider direction="vertical"></el-divider>
+                    <a index=''>帮助中心</a>
+              <a style="margin-left: 150px; color: #85ce61" href="javascript:void(0)" @click="index()">商城后台管理</a><el-divider direction="vertical"></el-divider>
+                    <a style="color: #8cc5ff" href="javascript:void(0)" @click="logout()">退出登录</a>
+                </span>
+        </div>
+      </el-header>
       <el-main>Main</el-main>
       <el-footer>Footer</el-footer>
     </el-container>
